caf99c7f9b996baf3a6f222a4c973e963cb4e901
skorlir <skorlir@gmail.com>
2015-02-01 13:05:17 -0600

    remove unneeded models

--- app/models/date-range.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-    , Schema = mongoose.Schema;
-
-var dateRange = new Schema({
-  start:  { type: Date, default:  Date.now },
-  end:    { type: Date }
-});
-
-module.exports = mongoose.model('DateRange', dateRange);
