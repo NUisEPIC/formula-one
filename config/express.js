46ffebe5b8e04fe229fd8f9b634ced279dbaefb0
skorlir <skorlir@gmail.com>
2015-02-12 12:48:35 -0600

    Fix invalid access-control-allow-origin header

--- config/express.js
+++ config/express.js
@@ -11,7 +11,7 @@ var methodOverride = require('method-override');
 var allowXDomainNuIsEpic = function(req, res, next) {
   // allow cross-origin-resource sharing from
   // any nuisepic.com domain
-  res.header('Access-Control-Allow-Origin', '*.nuisepic.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
