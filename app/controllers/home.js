0920a026177ff37d943c44e95d4b94292c65fb26
skorlir <skorlir@gmail.com>
2015-02-16 13:48:52 -0600

    add .env and mandrill api calls

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,10 +1,14 @@
 var express = require('express')
   , router = express.Router()
   , mongoose = require('mongoose')
-  , Program = require('../models/program.js')
+  , Program = require('../models/program.js').Program
   , Application = require('../models/application.js')
   , Response = require('../models/response.js').Response
-  , ObjectId = require('mongoose').Types.ObjectId;
+  , Person = require('../models/person.js').Person
+  , ObjectId = require('mongoose').Types.ObjectId
+  , sendConfirmationEmail = require('../../confirmation-mailer.js').sendConfirmationEmail;
+
+require('dotenv').load();
 
 module.exports = function (app) {
   app.use('/', router);
@@ -49,6 +53,14 @@ router.post('/:program/application', function(req, res) {
 
     console.log(req.body);
 
+    Person.create({
+      name: { first: req.body.name.first, last: req.body.name.last },
+      gender: req.body.gender
+    }, function(err, person) {
+      if(err) console.log(err);
+      else console.log("Person " + person.name.first + " " + person.name.last + " successfully created.");
+    });
+
     Response.create({
       raw: req.body
     }, function(err, newResponse) {
@@ -57,7 +69,24 @@ router.post('/:program/application', function(req, res) {
                                   ,{$push: {responses: newResponse}})
       .exec(function(err, application) {
         if(err) console.log(err) && res.send(500, 'Error executing query');
+        sendConfirmationEmail({
+          user: {
+            name: {
+              first: req.body.name.first,
+              last: req.body.name.last,
+              full: req.body.name.first + " " + req.body.name.last},
+            email: req.body.email,
+            hasAccount: false
+          },
+          account: {
+            setupLink: "#",
+            resetLink: "#",
+            loginLink: "http://epic-talent-portal.herokuapp.com/#/login"
+          }
+        }, function() {}, function() {});
         res.send('Looks successful enough.');
+
+        // TODO: add post('save') callback to Responses where if document.isNew, send verification email
       });
     });
   });
--- app/plugins/schema-tools.js
+++ app/plugins/schema-tools.js
@@ -1,7 +1,10 @@
 
 module.exports.requireAll = function(schema) {
-  Object.keys(schema.paths).forEach(function(key) {
-    schema.paths[key]['isRequired'] = true;
-    schema.tree[key]['required'] = true;
-  });
+    console.log(schema.tree);
+    console.log("\n==--==--==--==--==\n");
+  for (var p in schema.paths) {
+    console.log(p);
+    //console.log(schema.path(p));
+    if (p.indexOf('_') != 0) schema.path(p).required(true);
+  }
 }
