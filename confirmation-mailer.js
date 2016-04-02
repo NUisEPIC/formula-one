afb99738224a40afd6537e5f9152370a373e76da
skorlir <skorlir@gmail.com>
2016-04-02 07:20:00 -0500

    Change confirmation email

--- confirmation-mailer.js
+++ confirmation-mailer.js
@@ -6,7 +6,7 @@ exports.sendConfirmationEmail = function(templateData, success, failure) {
     "template_name": "EPIC Recruitment Application Response",
     "template_content" : {}, // there are no mc:edit fields
     "message": {
-      "subject": "Thanks for applying for EPIC, " + templateData.name + "!",
+      "subject": "Thanks for applying to EPIC, " + templateData.name + "!",
       "to": [{ "email": templateData.email,
                 "name": templateData.name
              }],
@@ -21,6 +21,8 @@ exports.sendConfirmationEmail = function(templateData, success, failure) {
           "vars": [
             { "name": "name",
               "content": templateData.name},
+            { "name": "epicTeam",
+              "content": templateData.applyingFor === 'EPIC Teams'}
           ]
         }
       ],
@@ -81,4 +83,3 @@ exports.sendStartupEmail = function(templateData, success, failure) {
       if(typeof(failure) == 'function') failure(error);
   });
 }
-
