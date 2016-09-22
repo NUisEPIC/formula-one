c1c6ece934075cd7cc337d66f3dc1c69a724fbd1
Murfalo <murphycangelo@u.northwestern.edu>
2016-09-21 22:51:17 -0500

    Recycle API key and remove from vc

--- mailer.js
+++ mailer.js
@@ -1,4 +1,6 @@
-var mailgun = require('mailgun-js')({apiKey: "key-0427e9f62f466535f9d41e5f8d45184f", domain: "nuisepic.com"})
+require('dotenv').load()
+
+var mailgun = require('mailgun-js')({apiKey: process.env.API_KEY, domain: "nuisepic.com"})
 var accord  = require('accord')
 
 /// Options: object containing template, variables, engine
